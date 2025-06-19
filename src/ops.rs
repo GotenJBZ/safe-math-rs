98776c80cfbc3e331de142db995945c194c1bd07
marco ilardi <marco@freemaker.it>
2025-06-20 00:00:10 +0200

    avoid verbose errors

--- src/ops.rs
+++ src/ops.rs
@@ -3,31 +3,41 @@ use std::ops::{Add, Div, Mul, Rem, Sub};
 
 pub use num_traits::ops::checked::{CheckedAdd, CheckedDiv, CheckedMul, CheckedRem, CheckedSub};
 
-#[diagnostic::on_unimplemented(message = "`{Self}` does not implement `SafeAdd`; \
-               add `add` to `#[SafeMathOps(...)]`")]
+#[diagnostic::on_unimplemented(
+    message = "Type `{Self}` cannot perform safe addition.",
+    note = "Add `add` to `#[SafeMathOps(...)]` when deriving `SafeMathOps`."
+)]
 pub trait SafeAdd: Copy + CheckedAdd + Add<Output = Self> {
     fn safe_add(self, rhs: Self) -> SafeMathResult<Self>;
 }
-#[diagnostic::on_unimplemented(message = "`{Self}` does not implement `SafeSub`; \
-               add `sub` to `#[SafeMathOps(...)]`")]
+#[diagnostic::on_unimplemented(
+    message = "Type `{Self}` cannot perform safe subtraction.",
+    note = "Add `sub` to `#[SafeMathOps(...)]` when deriving `SafeMathOps`."
+)]
 pub trait SafeSub: Copy + CheckedSub + Sub<Output = Self> {
     fn safe_sub(self, rhs: Self) -> SafeMathResult<Self>;
 }
 
-#[diagnostic::on_unimplemented(message = "`{Self}` does not implement `SafeMul`; \
-               add `mul` to `#[SafeMathOps(...)]`")]
+#[diagnostic::on_unimplemented(
+    message = "Type `{Self}` cannot perform safe multiplication.",
+    note = "Add `mul` to `#[SafeMathOps(...)]` when deriving `SafeMathOps`."
+)]
 pub trait SafeMul: Copy + CheckedMul + Mul<Output = Self> {
     fn safe_mul(self, rhs: Self) -> SafeMathResult<Self>;
 }
 
-#[diagnostic::on_unimplemented(message = "`{Self}` does not implement `SafeDiv`; \
-               add `div` to `#[SafeMathOps(...)]`")]
+#[diagnostic::on_unimplemented(
+    message = "Type `{Self}` cannot perform safe division.",
+    note = "Add `div` to `#[SafeMathOps(...)]` when deriving `SafeMathOps`."
+)]
 pub trait SafeDiv: Copy + CheckedDiv + Div<Output = Self> {
     fn safe_div(self, rhs: Self) -> SafeMathResult<Self>;
 }
 
-#[diagnostic::on_unimplemented(message = "`{Self}` does not implement `SafeRem`; \
-               add `rem` to `#[SafeMathOps(...)]`")]
+#[diagnostic::on_unimplemented(
+    message = "Type `{Self}` cannot perform safe remainder operation.",
+    note = "Add `rem` to `#[SafeMathOps(...)]` when deriving `SafeMathOps`."
+)]
 pub trait SafeRem: Copy + CheckedRem + Rem<Output = Self> {
     fn safe_rem(self, rhs: Self) -> SafeMathResult<Self>;
 }
